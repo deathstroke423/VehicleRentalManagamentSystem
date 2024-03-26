2f3b14cab98640214ac28bca00d7d12c58acf1f3
deathstroke423 <67819420+deathstroke423@users.noreply.github.com>
2024-03-25 21:29:22 -0400

    Final Working Update

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating instances of vehicles
+        Car car = new Car("Civic", "Honda", 2022, 50.00m, 5, "Petrol", "Automatic", false);
+        Truck truck = new Truck("F-150", "Ford", 2020, 80.00m, 2, "Pickup", true);
+        Motorcycle motorcycle = new Motorcycle("Ninja 300", "Kawasaki", 2021, 30.00m, 300, "Petrol", true);
+
+        // Displaying vehicle details
+        Console.WriteLine("Car Details:");
+        car.DisplayDetails();
+        Console.WriteLine();
+
+        Console.WriteLine("Truck Details:");
+        truck.DisplayDetails();
+        Console.WriteLine();
+
+        Console.WriteLine("Motorcycle Details:");
+        motorcycle.DisplayDetails();
+        Console.WriteLine();
+
+        // Testing RentalAgency
+        RentalAgency agency = new RentalAgency(3);
+        agency.AddVehicle(car, 0);
+        agency.AddVehicle(truck, 1);
+        agency.AddVehicle(motorcycle, 2);
+
+        while (true)
+        {
+            Console.WriteLine("Select a vehicle to rent (1 - Car, 2 - Truck, 3 - Motorcycle, 0 - Exit):");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 0)
+                break;
+
+            Console.WriteLine("Enter number of days to rent:");
+            int days = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    agency.RentVehicle(0, days); // Rent car
+                    break;
+                case 2:
+                    agency.RentVehicle(1, days); // Rent truck
+                    break;
+                case 3:
+                    agency.RentVehicle(2, days); // Rent motorcycle
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+        }
+
+        Console.WriteLine($"Total revenue of the rental agency: {agency.TotalRevenue:C}");
+    }
+}
---
+++ RentalAgency.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class RentalAgency
+{
+    private Vehicle[] Fleet;
+    public decimal TotalRevenue { get; private set; }
+
+    // Constructor
+    public RentalAgency(int fleetSize)
+    {
+        Fleet = new Vehicle[fleetSize];
+        TotalRevenue = 0;
+    }
+
+    // Method to add vehicles to the fleet
+    public void AddVehicle(Vehicle vehicle, int index)
+    {
+        Fleet[index] = vehicle;
+        Console.WriteLine($"Added {vehicle.Model} to the fleet.");
+    }
+
+    // Method to remove vehicles from the fleet
+    public void RemoveVehicle(int index)
+    {
+        if (Fleet[index] != null)
+        {
+            Console.WriteLine($"Removed {Fleet[index].Model} from the fleet.");
+            Fleet[index] = null;
+        }
+        else
+        {
+            Console.WriteLine("No vehicle found at the specified index.");
+        }
+    }
+
+    // Method to rent a vehicle
+    public void RentVehicle(int index, int days)
+    {
+        if (Fleet[index] != null)
+        {
+            TotalRevenue += Fleet[index].RentalPrice * days;
+            Console.WriteLine($"Vehicle {Fleet[index].Model} rented for {days} days.");
+            RemoveVehicle(index);
+        }
+        else
+        {
+            Console.WriteLine("Vehicle not available for rent.");
+        }
+    }
+}
